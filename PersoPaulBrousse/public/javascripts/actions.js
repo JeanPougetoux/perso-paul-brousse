60188a991e4c95b43687248bb94b3d95c718f781
JeanPougetoux <spyroo913@hotmail.com>
2018-07-26 01:33:29 +0200

    push modify/up/down

--- PersoPaulBrousse/public/javascripts/actions.js
+++ PersoPaulBrousse/public/javascripts/actions.js
@@ -21,8 +21,8 @@ $(document).ready(function(){
                     swal("Erreur", "Une erreur est survenue pendant la création de l'élément de navigation", "error");
                 },
                 success: function (msg) {
-                    swal("Félicitation", "Votre élément de navigation a bien été ajouté !", "success").then(function(){
-                      location.reload();  
+                    swal("Félicitation", msg.success, "success").then(function(){
+                        location.reload();  
                     });
                 }
             });
@@ -34,7 +34,7 @@ $(document).ready(function(){
             }
         });
     });
-    
+
     $(".add-sub-nav-element").click(function(){
         swal({
             text: 'Donnez un nom à votre nouveau sous-élément de navigation',
@@ -58,8 +58,8 @@ $(document).ready(function(){
                     swal("Erreur", "Une erreur est survenue pendant la création du sous-élément de navigation", "error");
                 },
                 success: function (msg) {
-                    swal("Félicitation", "Votre sous-élément de navigation a bien été ajouté !", "success").then(function(){
-                      location.reload();  
+                    swal("Félicitation", msg.success, "success").then(function(){
+                        location.reload();  
                     });
                 }
             });
@@ -68,6 +68,228 @@ $(document).ready(function(){
                 swal("Erreur", "Une erreur est arrivée !", "error");
             } else {
                 swal.close();
+            }
+        });
+    });
+
+    $(".modify-nav-element").click(function(){
+        swal({
+            text: 'Donnez le nouveau nom de votre élément de navigation',
+            content: "input",
+            button: {
+                text: "Valider",
+            }
+        }).then(name => {
+            if(!name){
+                return;
+            }
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    'newname': name,
+                    'navelementid': $(this).find("input").val()
+                },
+                url: "/admin/gestion/navelement/modify",
+                error: function (jqXHR, textStatus, errorThrown) {
+                    swal("Erreur", "Une erreur est survenue pendant le changement du nom de l'élément de navigation", "error");
+                },
+                success: function (msg) {
+                    swal("Félicitation", msg.success, "success").then(function(){
+                        location.reload();  
+                    });
+                }
+            });
+        }).catch(err => {
+            if (err) {
+                swal("Erreur", "Une erreur est arrivée !", "error");
+            } else {
+                swal.close();
+            }
+        });
+    });
+
+    $(".modify-sub-nav-element").click(function(){
+        swal({
+            text: 'Donnez le nouveau nom de votre sous-élément de navigation',
+            content: "input",
+            button: {
+                text: "Valider",
+            }
+        }).then(name => {
+            if(!name){
+                return;
+            }
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    'newname': name,
+                    'navsubelementid': $(this).find("input").val()
+                },
+                url: "/admin/gestion/navsubelement/modify",
+                error: function (jqXHR, textStatus, errorThrown) {
+                    swal("Erreur", "Une erreur est survenue pendant le changement du nom de l'élément de sous-navigation", "error");
+                },
+                success: function (msg) {
+                    swal("Félicitation", msg.success, "success").then(function(){
+                        location.reload();  
+                    });
+                }
+            });
+        }).catch(err => {
+            if (err) {
+                swal("Erreur", "Une erreur est arrivée !", "error");
+            } else {
+                swal.close();
+            }
+        });
+    });
+
+    $(".delete-nav-element").click(function(){
+        swal({
+            title: "Etes-vous sûr ?",
+            text: "Voulez-vous vraiment supprimer l'élément de navigation ?",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((willDelete) => {
+            if(!willDelete){
+                return;
+            }
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    'navelementid': $(this).find("input").val()
+                },
+                url: "/admin/gestion/navelement/delete",
+                error: function (jqXHR, textStatus, errorThrown) {
+                    swal("Erreur", "Une erreur est survenue pendant la suppression de l'élément de navigation", "error");
+                },
+                success: function (msg) {
+                    swal("Félicitation", msg.success, "success").then(function(){
+                        location.reload();  
+                    });
+                }
+            });
+        }).catch(err => {
+            if (err) {
+                swal("Erreur", "Une erreur est arrivée !", "error");
+            } else {
+                swal.close();
+            }
+        });
+    });
+
+    $(".delete-sub-nav-element").click(function(){
+        swal({
+            title: "Etes-vous sûr ?",
+            text: "Voulez-vous vraiment supprimer l'élément de sous-navigation ?",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((willDelete) => {
+            if(!willDelete){
+                return;
+            }
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    'navsubelementid': $(this).find("input").val()
+                },
+                url: "/admin/gestion/navsubelement/delete",
+                error: function (jqXHR, textStatus, errorThrown) {
+                    swal("Erreur", "Une erreur est survenue pendant la suppression de l'élément de sous-navigation" + JSON.stringify(jqXHR), "error");
+                },
+                success: function (msg) {
+                    swal("Félicitation", msg.success, "success").then(function(){
+                        location.reload();  
+                    });
+                }
+            });
+        }).catch(err => {
+            if (err) {
+                swal("Erreur", "Une erreur est arrivée !", "error");
+            } else {
+                swal.close();
+            }
+        });
+    });
+
+    $(".up-nav-element").click(function(){
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                'navelementid': $(this).find("input").val()
+            },
+            url: "/admin/gestion/navelement/up",
+            error: function (jqXHR, textStatus, errorThrown) {
+                swal("Erreur", JSON.stringify(jqXHR), "error");
+            },
+            success: function (msg) {
+                swal("Félicitation", msg.success, "success").then(function(){
+                    location.reload();  
+                });
+            }
+        });
+    });
+    
+    $(".down-nav-element").click(function(){
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                'navelementid': $(this).find("input").val()
+            },
+            url: "/admin/gestion/navelement/down",
+            error: function (jqXHR, textStatus, errorThrown) {
+                swal("Erreur", JSON.stringify(jqXHR), "error");
+            },
+            success: function (msg) {
+                swal("Félicitation", msg.success, "success").then(function(){
+                    location.reload();  
+                });
+            }
+        });
+    });
+    
+    $(".up-sub-nav-element").click(function(){
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                'navsubelementid': $(this).find("input").val()
+            },
+            url: "/admin/gestion/navsubelement/up",
+            error: function (jqXHR, textStatus, errorThrown) {
+                swal("Erreur", JSON.stringify(jqXHR), "error");
+            },
+            success: function (msg) {
+                swal("Félicitation", msg.success, "success").then(function(){
+                    location.reload();  
+                });
+            }
+        });
+    });
+    
+    $(".down-sub-nav-element").click(function(){
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                'navsubelementid': $(this).find("input").val()
+            },
+            url: "/admin/gestion/navsubelement/down",
+            error: function (jqXHR, textStatus, errorThrown) {
+                swal("Erreur", JSON.stringify(jqXHR), "error");
+            },
+            success: function (msg) {
+                swal("Félicitation", msg.success, "success").then(function(){
+                    location.reload();  
+                });
             }
         });
     });
--- PersoPaulBrousse/routes/admin.js
+++ PersoPaulBrousse/routes/admin.js
@@ -3,6 +3,8 @@ var bcrypt = require('bcrypt');
 var waterfall = require('async-waterfall');
 var models = require('../models');
 var router = express.Router();
+var sequelize = require('sequelize');
+var op = sequelize.Op;
 
 
 router.get('/connexion', function(req, res, next) {
@@ -63,28 +65,17 @@ router.get('/gestion', function(req, res, next){
             attributes: ['id', 'title', 'order'],
             order: [
                 ['order', 'ASC'],
+                [models.NavigationSubElement, 'order', 'ASC']
             ],
             include: [
                 { 
                     model: models.NavigationSubElement,
-                    order: [
-                        ['order', 'ASC'],
-                    ],
                     required: false,
-                    attributes: ['id', 'title', 'order'],
-                    include: [
-                        {
-                            model: models.PageContent,
-                            order: [
-                                ['order', 'ASC']
-                            ],
-                            required: false,
-                            attributes: ['id', 'content', 'type', 'order']
-                        }
-                    ]
+                    attributes: ['id', 'title', 'order']
                 }
             ]
         }).then(function(results){
+            console.log(JSON.stringify(results));
             res.render("pages/gestion-admin", {
                 structure: results 
             });
@@ -135,65 +126,266 @@ router.post('/gestion/navsubelement/add', function(req, res, next){
     });
 });
 
-router.post('/gestion', function(req, res, next){
+router.post('/gestion/navelement/modify', function(req, res, next){
     if(!req.session.connected){
-        res.redirect("/admin/connexion");
+        return res.status(500).json({'error': "Vous n'êtes pas connecté !"});
     }
-    else {
-        if(req.body.formtype && req.body.formtype == "addnavelement"){
-            models.NavigationElement.max('order').then(function(result){
-                var order = result ? result + 1 : 1;
-                models.NavigationElement.create({
-                    title: req.body.element,
-                    order: order
-                }).then(function(navelement){
-                    res.redirect("/admin/gestion");
-                })
-            });
-        }
-        else if(req.body.formtype && req.body.formtype == "addnavsubelement"){
-            models.NavigationSubElement.max('order', { where: { NavigationElementId: req.body.idparent } }).then(function(result){
-                var order = result ? result + 1 : 1;
-                models.NavigationSubElement.create({
-                    title: req.body.element,
-                    order: order,
-                    NavigationElementId: req.body.idparent
-                }).then(function(navsubelement){
-                    res.redirect("/admin/gestion");
-                })
-            });
-        }
-        else if(req.body.formtype && req.body.formtype == "deletenavelement"){
-            models.NavigationElement.findOne({
-                where: { id: req.body.id }
-            }).then(function(result){
-                result.destroy({force: true});
-                models.NavigationSubElement.findAll({
-                    where: { NavigationElementId: req.body.id }
-                }).then(function(results){
-                    results.forEach(function(result){
-                        result.destroy({force: true});
-                    });
-                    res.redirect("/admin/gestion");
-                }).catch(function(error){
 
-                })
-            }).catch(function(error){
-
-            });
-        }
-        else if(req.body.formtype && req.body.formtype == "deletenavsubelement"){
-            models.NavigationSubElement.findOne({
-                where: { id: req.body.id }
-            }).then(function(result){
-                result.destroy({force: true});
-                res.redirect("/admin/gestion");
-                return;
-            }).catch(function(error){
-
-            });
-        }
+    if(req.body.newname == null && req.body.navelementid == null){
+        return res.status(500).json({'error': "Il manque un paramètre"});
     }
+
+    models.NavigationElement.findOne({
+        where: { id: req.body.navelementid }
+    }).then(function(element){
+        if(element){
+            element.title = req.body.newname
+            element.save().then(function(){
+                return res.status(200).json({'success': "Le titre de l'élément de navigation a bien été changé"});
+            });
+        } else {
+            return res.status(404).json({'error': "L'élément de navigation correspondant à cet id n'a pas été trouvé"});     
+        }
+    }).catch(function(error){
+
+    });
+});
+
+router.post('/gestion/navsubelement/modify', function(req, res, next){
+    if(!req.session.connected){
+        return res.status(500).json({'error': "Vous n'êtes pas connecté !"});
+    }
+
+    if(req.body.newname == null && req.body.navsubelementid == null){
+        return res.status(500).json({'error': "Il manque un paramètre"});
+    }
+
+    models.NavigationSubElement.findOne({
+        where: { id: req.body.navsubelementid }
+    }).then(function(element){
+        if(element){
+            element.title = req.body.newname
+            element.save().then(function(){
+                return res.status(200).json({'success': "Le titre de l'élément de sous-navigation a bien été changé"});
+            });
+        } else {
+            return res.status(404).json({'error': "L'élément de sous-navigation correspondant à cet id n'a pas été trouvé"});     
+        }
+    }).catch(function(error){
+
+    });
+});
+
+router.post('/gestion/navelement/delete', function(req, res, next){
+    if(!req.session.connected){
+        return res.status(500).json({'error': "Vous n'êtes pas connecté !"});
+    }
+
+    if(req.body.navelementid == null){
+        return res.status(500).json({'error': "Il manque un paramètre"});
+    }
+
+    models.NavigationElement.findOne({
+        where: { id: req.body.navelementid }
+    }).then(function(result){
+        result.destroy({force: true});
+        models.NavigationSubElement.findAll({
+            where: { NavigationElementId: req.body.navelementid }
+        }).then(function(results){
+            results.forEach(function(result){
+                result.destroy({force: true});
+            });
+            return res.status(200).json({'success': "L'élément de navigation a bien été supprimé"});
+        }).catch(function(error){
+
+        })
+    }).catch(function(error){
+
+    });
+});
+
+router.post('/gestion/navsubelement/delete', function(req, res, next){
+    if(!req.session.connected){
+        return res.status(500).json({'error': "Vous n'êtes pas connecté !"});
+    }
+
+    if(req.body.navsubelementid == null){
+        return res.status(500).json({'error': "Il manque un paramètre"});
+    }
+
+    models.NavigationSubElement.findOne({
+        where: { id: req.body.navsubelementid }
+    }).then(function(result){
+        result.destroy({force: true}).then(function(){
+            return res.status(200).json({'success': "L'élément de sous-navigation a bien été supprimé"});
+        });
+    }).catch(function(error){
+
+    });
+});
+
+router.post('/gestion/navelement/up', function(req, res, next){
+    if(!req.session.connected){
+        return res.status(500).json({'error': "Vous n'êtes pas connecté !"});
+    }
+
+    if(req.body.navelementid == null){
+        return res.status(500).json({'error': "Il manque un paramètre"});
+    }
+
+    models.NavigationElement.findOne({
+        where: { id: req.body.navelementid }
+    }).then(function(element){
+        if(element){
+            models.NavigationElement.max('order', { where: { order: { [op.lt]: element.order } } }).then(max => {
+               models.NavigationElement.findOne({
+                   where: { order: max }
+               }).then(function(secondElement){
+                  var temp = JSON.parse(JSON.stringify(element.order));
+                   var temp2 = JSON.parse(JSON.stringify(secondElement.order));
+                   element.order = -1;
+                   element.save().then(function(){
+                       secondElement.order = temp;
+                       secondElement.save().then(function(){
+                          element.order = temp2;
+                           element.save().then(function(){
+                               return res.status(200).json({'success': "Les deux éléments de navigation ont bien été swappés"});
+                           })
+                       });
+                   });
+               }).catch(function(error){
+                   
+               });
+            });
+        } else {
+            return res.status(404).json({'error': "L'élément de navigation correspondant à cet id n'a pas été trouvé"});     
+        }
+    }).catch(function(error){
+
+    });
+});
+
+router.post('/gestion/navelement/down', function(req, res, next){
+    if(!req.session.connected){
+        return res.status(500).json({'error': "Vous n'êtes pas connecté !"});
+    }
+
+    if(req.body.navelementid == null){
+        return res.status(500).json({'error': "Il manque un paramètre"});
+    }
+
+    models.NavigationElement.findOne({
+        where: { id: req.body.navelementid }
+    }).then(function(element){
+        if(element){
+            models.NavigationElement.min('order', { where: { order: { [op.gt]: element.order } } }).then(min => {
+               models.NavigationElement.findOne({
+                   where: { order: min }
+               }).then(function(secondElement){
+                  var temp = JSON.parse(JSON.stringify(element.order));
+                   var temp2 = JSON.parse(JSON.stringify(secondElement.order));
+                   element.order = -1;
+                   element.save().then(function(){
+                       secondElement.order = temp;
+                       secondElement.save().then(function(){
+                          element.order = temp2;
+                           element.save().then(function(){
+                               return res.status(200).json({'success': "Les deux éléments de navigation ont bien été swappés"});
+                           })
+                       });
+                   });
+               }).catch(function(error){
+                   
+               });
+            });
+        } else {
+            return res.status(404).json({'error': "L'élément de navigation correspondant à cet id n'a pas été trouvé"});     
+        }
+    }).catch(function(error){
+
+    });
+});
+
+router.post('/gestion/navsubelement/up', function(req, res, next){
+    if(!req.session.connected){
+        return res.status(500).json({'error': "Vous n'êtes pas connecté !"});
+    }
+
+    if(req.body.navsubelementid == null){
+        return res.status(500).json({'error': "Il manque un paramètre"});
+    }
+
+    models.NavigationSubElement.findOne({
+        where: { id: req.body.navsubelementid }
+    }).then(function(element){
+        if(element){
+            models.NavigationSubElement.max('order', { where: { order: { [op.lt]: element.order } }, NavigationElementId: element.NavigationElementId }).then(max => {
+               models.NavigationSubElement.findOne({
+                   where: { order: max, NavigationElementId: element.NavigationElementId }
+               }).then(function(secondElement){
+                  var temp = JSON.parse(JSON.stringify(element.order));
+                   var temp2 = JSON.parse(JSON.stringify(secondElement.order));
+                   element.order = -1;
+                   element.save().then(function(){
+                       secondElement.order = temp;
+                       secondElement.save().then(function(){
+                          element.order = temp2;
+                           element.save().then(function(){
+                               return res.status(200).json({'success': "Les deux éléments de sous-navigation ont bien été swappés"});
+                           })
+                       });
+                   });
+               }).catch(function(error){
+                   
+               });
+            });
+        } else {
+            return res.status(404).json({'error': "L'élément de sous-navigation correspondant à cet id n'a pas été trouvé"});     
+        }
+    }).catch(function(error){
+
+    });
+});
+
+router.post('/gestion/navsubelement/down', function(req, res, next){
+    if(!req.session.connected){
+        return res.status(500).json({'error': "Vous n'êtes pas connecté !"});
+    }
+
+    if(req.body.navsubelementid == null){
+        return res.status(500).json({'error': "Il manque un paramètre"});
+    }
+
+    models.NavigationSubElement.findOne({
+        where: { id: req.body.navsubelementid }
+    }).then(function(element){
+        if(element){
+            models.NavigationSubElement.min('order', { where: { order: { [op.gt]: element.order } }, NavigationElementId: element.NavigationElementId }).then(min => {
+               models.NavigationSubElement.findOne({
+                   where: { order: min, NavigationElementId: element.NavigationElementId }
+               }).then(function(secondElement){
+                  var temp = JSON.parse(JSON.stringify(element.order));
+                   var temp2 = JSON.parse(JSON.stringify(secondElement.order));
+                   element.order = -1;
+                   element.save().then(function(){
+                       secondElement.order = temp;
+                       secondElement.save().then(function(){
+                          element.order = temp2;
+                           element.save().then(function(){
+                               return res.status(200).json({'success': "Les deux éléments de sous-navigation ont bien été swappés"});
+                           })
+                       });
+                   });
+               }).catch(function(error){
+                   
+               });
+            });
+        } else {
+            return res.status(404).json({'error': "L'élément de sous-navigation correspondant à cet id n'a pas été trouvé"});     
+        }
+    }).catch(function(error){
+
+    });
 });
 
 router.get('/', function(req, res, next){
