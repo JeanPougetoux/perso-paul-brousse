8f87d26ea75b3fb58eaa9c1a1e8e01725c9053a1
Jean Pougetoux <Jean.Pougetoux@fmc-ag.com>
2018-08-08 16:02:04 +0200

    adding display article

--- PersoPaulBrousse/public/javascripts/user-actions.js
+++ PersoPaulBrousse/public/javascripts/user-actions.js
@@ -24,4 +24,27 @@ $(document).ready(function(){
             });
         }
     });
+    
+    $(".access-content").on('mousedown', function () {
+        var id = $(this).find("input.id").val();
+        var type = $(this).find("input.type").val();
+        
+        if(type.localeCompare("LINK") == 0){
+            $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: "/article/" + id,
+            context: this,
+            error: function (jqXHR, textStatus, errorThrown) {
+                swal("Erreur", jqXHR.responseJSON.error, "error");
+            },
+            success: function (msg) {
+                $(this).attr("href", "/public/images/" + msg.success);
+                $(this).attr("download", msg.success);
+            }
+            });
+        } else {
+            $(this).attr("href", id);
+        }
+    });
 });
--- PersoPaulBrousse/routes/commune.js
+++ PersoPaulBrousse/routes/commune.js
@@ -2,12 +2,96 @@ var express = require('express');
 var models = require('../models');
 var router = express.Router();
 
+router.get('/article/:idArticle', function(req, res, next){
+    models.PageListElement.findOne({
+        where: { id: req.params.idArticle }
+    }).then(function(article){
+        if(article){
+            return res.status(200).json({'success': article.content});
+        } else {
+            return res.status(404).json({'error': "L'article demandé n'existe pas"});
+        }
+    })
+});
+
+router.get('/:page/:subpage/:article', function(req, res, next){
+    if(req.params.page.localeCompare(req.params.subpage) == 0){
+        next();
+    }
+    models.NavigationElement.findAll({
+        attributes: ['id', 'title', 'order'],
+        order: [
+            ['order', 'ASC'],
+            [models.NavigationSubElement, 'order', 'ASC']
+        ],
+        include: [
+            { 
+                model: models.NavigationSubElement,
+                required: false,
+                attributes: ['id', 'title', 'order', 'type'],
+                include: [
+                    {
+                        model: models.PageLink,
+                        required: false,
+                        attributes: ['link']
+                    }
+                ]
+            }
+        ]
+    }).then(function(results){
+        models.NavigationElement.findOne({
+            where: { title: req.params.page }
+        }).then(function(ne){
+            if(ne){
+                models.NavigationSubElement.findOne({
+                    where: {
+                        title: req.params.subpage,
+                        NavigationElementId: ne.id
+                    }
+                }).then(function(nse){
+                    if(nse){
+                        models.PageListElement.findOne({
+                            where: {
+                                id: req.params.article,
+                                NavigationSubElementId: nse.id,
+                                type: "CONTENT"
+                            }
+                        }).then(function(article){
+                           if(article){
+                               res.render("pages/commune", {
+                                    structure: results,
+                                    type: "ARTICLE",
+                                    content: article.content
+                                })
+                           } else {
+                               next(); // 404
+                           }
+                        }).catch(function(error){
+                            
+                        });
+                    } else {
+                        next(); // 404
+                    }
+                }).catch(function(error){
+
+                })
+            } else {
+                next(); // 404
+            }
+        }).catch(function(error){
+
+        });
+    }).catch(function(error){
+
+    });
+});
+
 router.get('/:page/:subpage', function(req, res, next){
     if(req.params.page.localeCompare(req.params.subpage) == 0){
         next();
     }
     models.NavigationElement.findAll({
-        ttributes: ['id', 'title', 'order'],
+        attributes: ['id', 'title', 'order'],
         order: [
             ['order', 'ASC'],
             [models.NavigationSubElement, 'order', 'ASC']
@@ -54,7 +138,8 @@ router.get('/:page/:subpage', function(req, res, next){
 
                         } else if(nse.type.localeCompare("LIST") == 0){
                             models.PageListElement.findAll({
-                                where: { NavigationSubElementId: nse.id }
+                                where: { NavigationSubElementId: nse.id },
+                                attributes: ["id", "illustration", "title", "description", "type"]
                             }).then(function(ples){
                                 res.render("pages/commune", {
                                     structure: results,
